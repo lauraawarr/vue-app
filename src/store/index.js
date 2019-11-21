d9380b69e72fc4e5f6086ae6a6389270a46cdb4f
Laura Warr <lauraawarr@gmail.com>
2019-11-21 01:56:59 -0800

    Replace fetchData function with api call

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
-import rawData from '../assets/data.json';
+import axios from 'axios'
 
 Vue.use(Vuex)
 
@@ -16,17 +15,9 @@ export default new Vuex.Store({
   },
   actions: {
     fetchData ({ commit }) {
-      const keys = Object.keys(rawData[0]);
-      const sortBy = keys[Math.floor(Math.random() * keys.length)];
-      const startSlice = Math.floor(Math.random() * rawData.length);
-      const endSlice = startSlice + Math.ceil(Math.random() * rawData.length);
-      const randomData = rawData
-        .sort((a, b) => {
-          if (a[sortBy] === b[sortBy]) return 0;
-          return a[sortBy] > b[sortBy] ? 1 : -1;
-        })
-        .slice(startSlice, endSlice);
-      commit('updateDataset', randomData);
+      axios.get('https://randomdataprovider.azurewebsites.net/api/DataPoints/random')
+        .then(res => commit('updateDataset', res.data))
+        .catch();
     },
   },
   modules: {
